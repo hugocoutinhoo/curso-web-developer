270b959b69e62eecaafb63b63523e64b329787de
Hugo Coutinho <109985122+hugocoutinhoo@users.noreply.github.com>
2023-02-15 15:09:16 -0300

    .

---
+++ secao-3_javascript/1_JS-basico/tipos-de-dados/5-empty-values/script.js
@@ -0,0 +1,8 @@
+console.log(null)
+console.log(undefined);
+
+/*Conversão de tipo automática*/
+console.log(5 * null);
+console.log("5" - 3);
+console.log(typeof ("5" - 3));
+console.log("asdf" * 22);
